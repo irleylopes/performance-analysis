38ff2cd6502d0273a718f7b0eae50cc1dc117377
Irley Lopes <irleylopes@gmail.com>
2018-11-12 21:47:40 -0200

    inicio projeto

---
+++ src/main/java/br/com/performanceAnalysis/performanceanalysis/PerformanceAnalysisApplication.java
@@ -0,0 +1,12 @@
+package br.com.performanceAnalysis.performanceanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceAnalysisApplication.class, args);
+	}
+}
